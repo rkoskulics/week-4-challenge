6c6ff2f40ff1f1383d19c9ee7d014c247df7105c
Rob <robert.koskulics@me.com>
2017-11-27 18:20:39 -0500

    Minor Changes

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+var attacker = "";
+
+	function moveWarrior() {
+		$("button").detach().appendTo("#row2");
+	};
+
+	$("button").on("click", function(){
+		attacker = this.value;
+		moveWarrior(attacker);
+		console.log(this);
+	})
+
 
 
 
